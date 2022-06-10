1200082ebb2f2aff5d0bbd6758e530ec8f4816e5
Arseniy <75582699+selfiction@users.noreply.github.com>
2022-06-11 04:22:49 +0500

    updated
    added working HEADLESS MODE, better optimization, logging.

--- script.py
+++ script.py
@@ -1,23 +1,28 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 
 
 
 
+chrome_options = Options()
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\hp\Desktop\proj\chromedriver.exe')
-options = webdriver.ChromeOptions()
-options.add_argument("headless")
 
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=chrome_options)
 
 def ref():
+    global profile_name
+    global profile_status
     profile_id = input('Введите ID профиля: ')
     url = f'https://vk.com/{profile_id}'
     driver.get(url)
     try:
-        profile_name = driver.find_element_by_xpath('//*[@id="page_info_wrap"]/div[1]/h1').text
-        profile_status = driver.find_element_by_xpath('//*[@id="page_info_wrap"]/div[1]/div[1]/div').text
+        profile_name = driver.find_element(By.XPATH, '//*[@id="page_info_wrap"]/div[1]/h1').text
+        profile_status = driver.find_element(By.XPATH, '//*[@id="page_info_wrap"]/div[1]/div[1]/div').text
     except:
         print('Ошибка, профиль может быть удалён либо закрыт!')
         ref()
@@ -26,8 +31,9 @@ def ref():
     driver.refresh()
 
     f = open('info.txt', mode='a')
-    f.write(f"Профиль: {profile_name}"+ "\n" + f"Статус профиля: {profile_status}" + "\n" + f"ID профиля: {profile_id}" + "\n" )
+    f.write(f"Профиль: {profile_name}"+ "\n" + f"Статус профиля: {profile_status}" + "\n" + f"ID профиля: {profile_id}" + "\n" + f"Время проверки: {time_string}" + "\n" + "\n")
     f.close()
+    driver.quit()
 
 
 while True:
